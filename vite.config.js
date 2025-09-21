5d0542a216efd4856017cd27c43dab18d1b063e2
klu2300031244 <2300031244@kluniversity.in>
2025-09-21 22:08:02 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +8,7 @@ export default defineConfig({
       '/employees': {
         target: 'http://localhost:8080',
         changeOrigin: true,
-        secure: false,
+        secure: false
       }
     }
   }
