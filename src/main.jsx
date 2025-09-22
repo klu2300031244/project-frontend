3f6f8d16b42d11e68406fdc390f11fd3a90de53a
klu2300031244 <2300031244@kluniversity.in>
2025-09-22 08:51:27 +0530

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </BrowserRouter>
-);
-import { BrowserRouter } from 'react-router-dom';
+  </StrictMode>,
+)
