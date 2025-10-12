21036c69311338090f431aa5c096b0ce84251e89
klu2300031244 <2300031244@kluniversity.in>
2025-10-12 20:57:41 +0530

    docker

--- Frontend/Employee-Client/vite.config.js
+++ Frontend/Employee-Client/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ems/", // correct for subdirectory SPA routing
+  base: '/ems/', // correct for subdirectory SPA routing
   server: {
     proxy: {
       '/employees': {
